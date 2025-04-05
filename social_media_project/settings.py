dc81c35a2244d31626d01550490e470be52b32d0
Wendmagegn <wendmageg9412@gmail.com>
2025-04-05 20:51:44 +0300

    Uers app for user registration and login and also some setting on JWT Authontication setup

--- social_media_project/settings.py
+++ social_media_project/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_media',
     'rest_framework',
+    'users',
+    'rest_framework_simplejwt',
+    'social_media',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
