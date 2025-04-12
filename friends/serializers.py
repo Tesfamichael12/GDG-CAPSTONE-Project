6b50d732186de839ec6fd27932016a044f3059f1
Wendmagegn <wendmageg9412@gmail.com>
2025-04-12 19:29:32 +0300

    Some modification on friends app adding follow count object and serializers

--- friends/serializers.py
+++ friends/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from accounts.models import User
 from friends.models import Follow
 from users.serializers import UserSerializer
 
@@ -10,3 +11,18 @@ class FollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follow
         fields = '__all__'
+
+
+class UserFollowCountSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'followers_count', 'following_count']
+
+    def get_followers_count(self, obj):
+        return obj._followers.count()
+
+    def get_following_count(self, obj):
+        return obj._following.count()
