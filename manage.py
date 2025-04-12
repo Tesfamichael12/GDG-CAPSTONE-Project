099a5bf8f20e1ce422c441f9604bad00dd706ccd
haregewey <haregetewabe47@gmail.com>
2025-04-12 04:09:34 +0300

    to add manage.py files

--- manage.py
+++ manage.py
@@ -6,17 +6,15 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_project.settings')
-=======
+   
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_app.settings')
->>>>>>> 5a21c4e (Add JWT authentication and protect home endpoint)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
+       "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
