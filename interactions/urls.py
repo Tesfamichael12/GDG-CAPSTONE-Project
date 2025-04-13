dda77bf5f2297422bee535f83e8027c4badaf63a
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2025-04-13 18:35:06 +0300

    remove from here follow path
    remove from here follow path then moved to friends urls
    
    Signed-off-by: Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>

--- interactions/urls.py
+++ interactions/urls.py
@@ -2,10 +2,7 @@ from django.urls import path, include  # 'include' is used below for including r
 from rest_framework.routers import DefaultRouter
 from .views import (
     CommentViewSet, 
-    LikeViewSet, 
-    FollowViewSet,
-    FollowersListView,
-    FollowingListView
+    LikeViewSet
 )
 
 comment_router = DefaultRouter()
@@ -15,8 +12,4 @@ urlpatterns = [
     path('posts/<int:post_id>/', include(comment_router.urls)),
     path('posts/<int:post_id>/like/', LikeViewSet.as_view({'post': 'like'}), name='post-like'),
     path('posts/<int:post_id>/unlike/', LikeViewSet.as_view({'post': 'unlike'}), name='post-unlike'),
-    path('users/<int:user_id>/follow/', FollowViewSet.as_view({'post': 'follow'}), name='user-follow'),
-    path('users/<int:user_id>/unfollow/', FollowViewSet.as_view({'post': 'unfollow'}), name='user-unfollow'),
-    path('users/<int:user_id>/followers/', FollowersListView.as_view(), name='user-followers'),
-    path('users/<int:user_id>/following/', FollowingListView.as_view(), name='user-following'),
 ]
