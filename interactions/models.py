e2d8aa050ef4d15b8c9ddbc03cdf1413109fc98c
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2025-04-13 18:29:55 +0300

    Use costom User and remove Follow models from here
    Use custom define User and remove Follow models and move to friends app models
    
    Signed-off-by: Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>

--- interactions/models.py
+++ interactions/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.conf import settings
+# from django.conf import settings
 from posts.models import Post
+from accounts.models import User
 
 class Comment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,7 +16,7 @@ class Comment(models.Model):
         return f"Comment by {self.user.username} on {self.post}"
 
 class Like(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
     
@@ -25,23 +26,3 @@ class Like(models.Model):
     
     def __str__(self):
         return f"Like by {self.user.username} on {self.post}"
-
-class Follow(models.Model):
-    follower = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        related_name='followed_users',  
-        on_delete=models.CASCADE
-    )
-    following = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        related_name='followers_set',  
-        on_delete=models.CASCADE
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        unique_together = ('follower', 'following')
-        ordering = ['-created_at']
-    
-    def __str__(self):
-        return f"{self.follower.username} follows {self.following.username}"
