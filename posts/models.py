dc124c3f92a4e49c35596b82906fa11407fbe17f
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2025-04-13 17:33:10 +0300

    Update models.py
    Use Custom User that define in accounts.models by importing User
    and add Validation on image input image type
    
    Signed-off-by: Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-from django.conf import settings
+from django.core.validators import FileExtensionValidator
+from accounts.models import User
 
 class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
-    image = models.ImageField(upload_to='post_images/', null=True, blank=True)  # Add this field
+    image = models.ImageField(
+        upload_to='post_images/', 
+        null=True, 
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'gif'])]
+    )   # Image field for post image 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
