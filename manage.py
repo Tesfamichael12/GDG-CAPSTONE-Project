d2411c9dc73e8c73456845a54513fc3df589baea
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-04-13 15:07:31 +0300

    Rename files referenced in [manage.py]

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ def main():
     """Run administrative tasks."""
 
    
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_project.settings')
 
     try:
         from django.core.management import execute_from_command_line
