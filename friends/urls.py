ecce15f59fd5e6f9755f872d44e1c11dd30d73ac
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2025-04-13 18:22:03 +0300

    Update urls.py
    correctly define url path for follow
    
    Signed-off-by: Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>

--- friends/urls.py
+++ friends/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import FollowViewSet, UserFollowCountViewSet
+from .views import FollowViewSet, FollowersListView, FollowingListView, UserFollowCountViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -9,4 +9,7 @@ router.register(r'user-follow-counts', UserFollowCountViewSet, basename='user-fo
 urlpatterns = [
     path('follow/<int:user_id>/', FollowViewSet.as_view({'post': 'create', 'delete': 'delete'}), name="follow"),
     path('user-follow-count/<int:user_id>/', UserFollowCountViewSet.as_view({'get': 'retrieve'}), name="user-follow-count"),
+    path('users/<int:user_id>/unfollow/', FollowViewSet.as_view({'post': 'unfollow'}), name='user-unfollow'),
+    path('users/<int:user_id>/followers/', FollowersListView.as_view(), name='user-followers'),
+    path('users/<int:user_id>/following/', FollowingListView.as_view(), name='user-following'),
 ]
